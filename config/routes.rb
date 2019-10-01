612da102ed4e4cf0e8f33dffe95643662335e932
Ashish Rao <ashishrao2598@gmail.com>
2019-10-01 23:09:37 +0530

    add users with tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :users, only: [:create, :edit, :update]
+  resources :sessions, only: [:create]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  root 'welcome#index'
+  root 'artists#index'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    id { 1 }
+    username { "MyString" }
+    password { "valleyforge" }
+  end
+end
