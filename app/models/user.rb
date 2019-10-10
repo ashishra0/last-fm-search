7eee59a42874bb19be2bbc7b5215406fc171c6c1
Ashish Rao <ashishrao2598@gmail.com>
2019-10-10 13:02:23 +0530

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   USERNAME_FORMAT = /\A[a-zA-Z0-9]+\z/.freeze
   has_secure_password
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 20}
   validates :username,
             presence: true,
             uniqueness: { case_sensitive: true },
